f998ca4b4dd8cd3d52f774c15d9fc43291ddac58
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2023-02-24 17:31:54 +0900

    [Gold V] Title: ABCDE, Time: 424 ms, Memory: 104380 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+// 자료구조가 Array of ArrayList
+public class Main {
+	static int N, M;
+	static Friend[] friends;
+	static boolean done;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		// Friend 자료구조 생성
+		friends = new Friend[N];
+		for (int i = 0; i < N; i++) {
+			friends[i] = new Friend(i);
+		}
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			friends[a].list.add(new Friend(b));
+			friends[b].list.add(new Friend(a));
+		}
+
+		// 모든사람 각각에 대해서 연속적으로 연결된 친구관계가 5명인 경우를 찾는다.
+		for (int i = 0; i < N; i++) {
+			dfs(i, 0, 1 << i); // 빝마스킹,,,
+			if (done) {
+				System.out.println(1);
+				return;
+			}
+		}
+		System.out.println(0);
+	}
+
+	static void dfs(int num, int cnt, int visit) {
+		// 연속적인 관계가 생기면 done = true 하고 return
+		if (done)
+			return;
+		// 기저조건
+		// 연속적으로 5명의 친구가 성립
+		if (cnt == 4) {
+			done = true;
+			return;
+		}
+
+		friends[num].list.forEach( (f) -> {
+			if ((visit & (1 << f.num)) == 0)
+				dfs(f.num, cnt + 1, visit | 1 << f.num);
+		});
+	}
+
+	static class Friend {
+		int num;
+		List<Friend> list = new ArrayList<>();
+
+		Friend(int num) {
+			this.num = num;
+		}
+	}
+}
