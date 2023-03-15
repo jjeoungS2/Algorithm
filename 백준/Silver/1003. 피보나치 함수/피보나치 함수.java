6d388cbb37aec844fa9355e1fcc6c2a6906a1031
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2023-03-15 10:02:13 +0900

    [Silver III] Title: 피보나치 함수, Time: 124 ms, Memory: 14008 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,29 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-// 피보나치 함수
 public class Main {
-	static int[][] cnt = new int[2][41];
+	static int[] cnt = new int[41];
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		cnt[0][0] = 1;
-		cnt[1][0] = 0;
-		cnt[0][1] = 0;
-		cnt[1][1] = 1;
+		cnt[0] = 0;
+		cnt[1] = 1;
 
 		for (int i = 2; i < 41; i++) {
-			cnt[0][i] = cnt[0][i-1]+cnt[0][i-2];	// 0개수
-			cnt[1][i] = cnt[1][i-1]+cnt[1][i-2];	// 1개수
+			cnt[i] = cnt[i-1]+cnt[i-2];	// 1개수
 			
 		}
 		for (int t = 1; t <= T; t++) {
 			int n = Integer.parseInt(br.readLine());
-			sb.append(cnt[0][n]).append(" ").append(cnt[1][n]).append("\n");
+			if(n == 0) {
+				sb.append(1).append(" ").append(0).append("\n");
+				continue;
+			}
+			sb.append(cnt[n-1]).append(" ").append(cnt[n]).append("\n");
 		}
 		System.out.println(sb);
 	}
-
 }
