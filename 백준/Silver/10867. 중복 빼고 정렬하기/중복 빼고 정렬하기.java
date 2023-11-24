e17773afa308bd83bb8d0d711357ef4eaf0ccb89
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2023-11-24 09:55:28 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 304 ms, Memory: 26224 KB -BaekjoonHub

--- 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.java
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.java
@@ -1,24 +1,33 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.*;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
 
 public class Main {
     static int N;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        TreeMap<Integer,Integer> arr = new TreeMap<>();
-
+        int[] arr = new int[N];
+        List<Integer> listA = new ArrayList<>();     // 초기 용량은 10
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0 ; i<N;i++){
-            arr.put(Integer.parseInt(st.nextToken()),0);
+        for(int i = 0; i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
+        Arrays.sort(arr);
+        int num = arr[0];
         StringBuilder sb = new StringBuilder();
-        for(Integer key: arr.keySet()){
-            System.out.print(key+" ");
+        sb.append(num+" ");
+        for(int i =1;i<N;i++){
+            if(arr[i]==num) continue;
+            else{
+                num = arr[i];
+                sb.append(num+" ");
+            }
         }
+        System.out.println(sb);
     }
 }
