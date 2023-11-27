57af2fdf597c2717d50c10d3d23b67b62930cb56
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2023-11-27 17:41:06 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 776 ms, Memory: 73096 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // 트리의 부모 찾기
@@ -17,9 +18,7 @@ public class Main {
         list = new ArrayList[N+1];
         parent = new int[N+1];
 
-        for (int i = 1; i <= N; i++) {
-            list[i] = new ArrayList<>();
-        }
+        Arrays.setAll(list, i -> new ArrayList<>());
 
         for(int i = 0; i<N-1;i++){
             st = new StringTokenizer(br.readLine());
@@ -31,9 +30,12 @@ public class Main {
         }
 
         dfs(1);
+
+        StringBuilder sb = new StringBuilder();
         for(int i = 2; i<N+1; i++){
-            System.out.println(parent[i]);
+            sb.append(parent[i]).append("\n");
         }
+        System.out.print(sb);
     }
     public static void dfs(int index){
         visit[index] = true;
