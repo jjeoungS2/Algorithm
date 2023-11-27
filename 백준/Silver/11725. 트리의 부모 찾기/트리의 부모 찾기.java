d088c45fa70b6699a409f2a014e89a76b2c69260
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2023-11-27 17:33:55 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 2184 ms, Memory: 89524 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+// 트리의 부모 찾기
+public class Main {
+    static int N;
+    static int[] parent;
+    static boolean[] visit;
+    static ArrayList<Integer> list[];
+    public static  void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        N = Integer.parseInt(br.readLine());
+        visit = new boolean[N+1];
+        list = new ArrayList[N+1];
+        parent = new int[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i<N-1;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list[a].add(b);
+            list[b].add(a);
+        }
+
+        dfs(1);
+        for(int i = 2; i<N+1; i++){
+            System.out.println(parent[i]);
+        }
+    }
+    public static void dfs(int index){
+        visit[index] = true;
+        for(int i : list[index]){
+            if(!visit[i]){
+                parent[i] = index;
+                dfs(i);
+            }
+        }
+    }
+}
