c7e715a6d9f7199c238c30c7df9d69940f034827
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2024-06-14 16:46:42 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 416 ms, Memory: 40488 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,41 +1,47 @@
 import java.util.*;
+import java.io.*;
+
 public class Main {
-    static int n,m;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static int[][] arr;
-    static int[][] dp;
-    static int max = 0;
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        n = scan.nextInt();
-        arr = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = scan.nextInt();
-            }
-        }
-        dp = new int[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                max = Math.max(dfs(i,j), max);
-            }
-        }
-        System.out.println(max);
-    }
-    public static int dfs(int x, int y) {
-        if (dp[x][y] != 0) {
-            return dp[x][y];
-        }
-        dp[x][y] = 1;
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if(nx >= 0 && ny >= 0 && nx < n && ny < n){
-                if(arr[nx][ny] > arr[x][y])
-                    dp[x][y] = Math.max(dp[x][y], dfs(nx,ny) + 1);
-            }
-        }
-        return dp[x][y];
-    }
+	static int n;
+	static int[][] list;
+	static int[][] dp;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	static int res = 0;
+ 	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		n = Integer.parseInt(br.readLine());
+		list = new int[n][n];
+		dp = new int[n][n];
+		
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < n; j++) {
+				list[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				res = Math.max(dfs(i,j),res);
+			}
+		}
+		System.out.println(res);
+	}
+ 	
+ 	private static int dfs(int x, int y) {
+ 		if(dp[x][y] != 0) return dp[x][y];
+ 		dp[x][y] = 1;
+ 		for(int i = 0; i < 4; i++) {
+ 			int nx = x + dx[i];
+ 			int ny = y + dy[i];
+ 			if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+ 			if(list[nx][ny] > list[x][y]) {
+ 				dp[x][y] = Math.max(dp[x][y], dfs(nx,ny)+1);
+ 			}
+ 		}
+ 		return dp[x][y];
+ 	}
+
 }
