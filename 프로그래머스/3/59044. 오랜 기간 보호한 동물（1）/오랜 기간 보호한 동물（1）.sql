bb140e4728ab9d8a6a25ad79023bbc8a1dd62477
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2024-10-31 01:26:45 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,7 +1,7 @@
 -- 코드를 입력하세요
-SELECT I.NAME, I.DATETIME
-from ANIMAL_INS as I left join ANIMAL_OUTS as O
-on I.ANIMAL_ID = O.ANIMAL_ID
-where O.ANIMAL_ID is null
-order by I.DATETIME
+SELECT i.NAME, i.DATETIME
+from ANIMAL_INS as i left join ANIMAL_OUTS as o
+on i.ANIMAL_ID = o.ANIMAL_ID
+where o.ANIMAL_ID is null
+order by i.DATETIME
 limit 3
